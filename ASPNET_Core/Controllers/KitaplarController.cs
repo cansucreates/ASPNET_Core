bc0b536b26a4b80fd240cabb5d54e9ae43a78661
cansucreates <caysagdic@gmail.com>
2024-05-29 12:16:08 +0300

    Kitaplar crud

--- ASPNET_Core/Controllers/KitaplarController.cs
+++ ASPNET_Core/Controllers/KitaplarController.cs
@@ -1,5 +1,7 @@
 ﻿using ASPNET_Core.Data;
+using ASPNET_Core.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ASPNET_Core.Controllers
 {
@@ -15,9 +17,58 @@ namespace ASPNET_Core.Controllers
 
         public IActionResult Index()
         {
+            return View(_db.Kitaplars.ToList());
+        }
+
+        //Kaydetme işlemi 
+        public IActionResult Create()
+        {
+            // we are creating a list of SelectListItem objects to use it in the view
+            // we are selecting the id of the Turlers table and adding it to the SelectListItem object
+            List<SelectListItem> turid_liste = (from x in _db.Turlers
+                                                select new SelectListItem{
+                                                    Value = x.id.ToString(),
+                                                    Text = x.KitapTur}).ToList();
+           ViewBag.TurId = turid_liste;
             return View();
         }
 
-        // 
+        [HttpPost]
+        public IActionResult Create(Kitaplar kitap)
+        {
+            _db.Kitaplars.Add(kitap);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Güncelleme işlemi
+        public IActionResult Edit(int id)
+        {
+            List<SelectListItem> turid_liste = (from x in _db.Turlers
+                                                select new SelectListItem
+                                                {
+                                                    Value = x.id.ToString(),
+                                                    Text = x.KitapTur
+                                                }).ToList();
+            ViewBag.TurId = turid_liste;
+            return View(_db.Kitaplars.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Kitaplar kitap)
+        {
+            _db.Kitaplars.Update(kitap);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // Silme işlemi (direk butonla silinecek)
+        public IActionResult Delete(int id)
+        {
+            var sil = _db.Kitaplars.Find(id);
+            _db.Kitaplars.Remove(sil);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }   
     }
 }
