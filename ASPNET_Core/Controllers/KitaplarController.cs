a16a029f6cc235fbac584622ec6dcfc9b4d64333
cansucreates <caysagdic@gmail.com>
2024-05-29 10:52:46 +0300

    turler crud

--- ASPNET_Core/Controllers/KitaplarController.cs
+++ ASPNET_Core/Controllers/KitaplarController.cs
@@ -6,16 +6,18 @@ namespace ASPNET_Core.Controllers
     public class KitaplarController : Controller
     {
         // we are creating a private readonly ApplicationDbContext object to use it in the controller
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _db;
         // we are creating a constructor that takes an ApplicationDbContext object as a parameter
-        public KitaplarController(ApplicationDbContext context)
+        public KitaplarController(ApplicationDbContext db)
         {
-            _context = context; // we are assigning the ApplicationDbContext object to the private readonly ApplicationDbContext object
+            _db = db; // we are assigning the ApplicationDbContext object to the private readonly ApplicationDbContext object
         }
 
         public IActionResult Index()
         {
             return View();
         }
+
+        // 
     }
 }
--- ASPNET_Core/Controllers/TurlerController.cs
+++ ASPNET_Core/Controllers/TurlerController.cs
@@ -1,20 +1,71 @@
 ﻿using ASPNET_Core.Data;
+using ASPNET_Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASPNET_Core.Controllers
 {
     public class TurlerController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        // mvcde yaptıgımız gibi modeldeki classtan nesne üretmiyoruz. Bunun yerine dependency injection yapıyoruz.
+        private readonly ApplicationDbContext _db;
 
-        public TurlerController(ApplicationDbContext context)
+        public TurlerController(ApplicationDbContext db)
         {
-            _context = context;
+            _db = db;
         }
 
+        // listeleme işlemi için index action'ı oluşturuyoruz.
         public IActionResult Index()
+        {
+            return View(_db.Turlers.ToList());
+        }
+
+        // Create action'ı oluşturuyoruz.
+        public IActionResult Create()
         {
             return View();
         }
+
+        // Create action'ı için post işlemi oluşturuyoruz.
+        [HttpPost]
+        public IActionResult Create(Turler tur)
+        {
+                _db.Turlers.Add(tur);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+        }
+
+        // Güncelle
+        public IActionResult Edit(int id)
+        {
+            // var guncelle = _db.Turlers.Where(i => i.id == id).FirstOrDefault();
+            var guncelle = _db.Turlers.Find(id); // find core'a özgü bir metot. id'ye göre veriyi getirir.
+            return View(guncelle);
+        }
+
+        // Güncelleme işlemi için post işlemi oluşturuyoruz.
+        [HttpPost]
+        public IActionResult Edit(int id, Turler tur)
+        {
+            _db.Turlers.Update(tur); // güncelleme işlemi için update metotunu kullanıyoruz.
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // Silme işlemi view'ı oluşturuyoruz.
+        public IActionResult Delete(int id)
+        {
+            return View(_db.Turlers.Find(id));
+        }
+
+        // Silme işlemi için post işlemi oluşturuyoruz.
+        [HttpPost]
+        public IActionResult Delete(int id,Turler tur)
+        {
+            _db.Turlers.Remove(tur); // silme işlemi için remove metotunu kullanıyoruz.
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
